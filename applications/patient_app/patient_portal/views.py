7ce12e88b8ad770c518c42c460cd230b574976f9
Suguru <suguru.ben@usp.br>
2024-03-21 01:07:26 -0300

    fix

--- applications/patient_app/patient_portal/views.py
+++ applications/patient_app/patient_portal/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'example.html')
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+def register(request):
+    return render(request, 'auth/register.html')
