78a775d160ddd095b2b44c13839d2293d186d9ae
Suguru <suguru.ben@usp.br>
2024-04-01 18:37:47 -0300

    :rocket: Introduced html structure and dashboard basic page

--- applications/patient_app/patient_portal/urls.py
+++ applications/patient_app/patient_portal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #path('', views.index, name='index'),
     path('', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- applications/patient_app/patient_portal/views.py
+++ applications/patient_app/patient_portal/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'example.html')
-
 def login(request):
     return render(request, 'auth/login.html')
 
 def register(request):
     return render(request, 'auth/register.html')
+
+def dashboard(request):
+    return render(request, 'services/dashboard.html')
