e39631b72f30a329c857d5323cae5ed157b4e38b
SanderSohngen <sander.sohngen@usp.br>
2024-04-08 23:38:57 -0300

    improved UX/UI

--- applications/nutritionist_app/nutritionist-app/src/components/Loading/Loading.jsx
+++ applications/nutritionist_app/nutritionist-app/src/components/Loading/Loading.jsx
@@ -1,9 +1,9 @@
-import { CircularProgress, Center } from '@chakra-ui/react';
+import { Spinner, Center } from '@chakra-ui/react';
 
 function Loading() {
   return (
-    <Center h="100vh">
-      <CircularProgress isIndeterminate color="customPalette.700" />
+    <Center>
+      <Spinner color="customPalette.700" thickness="4px" speed="0.65s" emptyColor="gray.200" size="xl" />
     </Center>
   );
 }
--- applications/nutritionist_app/nutritionist-app/src/layouts/MainLayout.jsx
+++ applications/nutritionist_app/nutritionist-app/src/layouts/MainLayout.jsx
@@ -1,12 +1,40 @@
-import { Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { Flex, useToast } from '@chakra-ui/react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 import { useNavigation } from 'react-router-dom';
 import Loading from '../components/Loading/Loading';
+import { useAuth } from '../context/AuthContext';
 
 function MainLayout() {
     const navigation = useNavigation();
+    const { user, isLoggedIn } = useAuth()
+    const toast = useToast();
+    const [hasWelcomed, setHasWelcomed] = useState(false);
+
+    useEffect(() => {
+        if (isLoggedIn && !hasWelcomed) {
+            toast({
+                title: 'Bem vindo de volta',
+                description: `Olá novamente, ${user.name}.`,
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+            })
+            setHasWelcomed(true);
+        }
+        else if (!isLoggedIn && hasWelcomed) {
+            toast({
+                title: 'Tchau',
+                description: `Esperamos que volte em breve`,
+                status: 'info',
+                duration: 5000,
+                isClosable: true,
+            })
+            setHasWelcomed(false);
+        }
+    }, [isLoggedIn, hasWelcomed])
 
     return (
         <Flex flexDirection="column" minHeight="100vh">
