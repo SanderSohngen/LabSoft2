838b7bdd597bd65e93481e6f0661f3514bc75f79
Suguru <suguru.ben@usp.br>
2024-04-10 16:46:59 -0300

    adjusted setting

--- applications/patient_app/patient_app/settings.py
+++ applications/patient_app/patient_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bu#ab11)mx@t!1g)gp^ia-wnf8kup4kzy736@2i!qmyr2(=u#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
