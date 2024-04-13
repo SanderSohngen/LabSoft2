99f59710ba3b1a93c666eb22ee3e829d1b061433
Ubuntu <ubuntu@ip-172-31-92-248.ec2.internal>
2024-04-13 13:44:50 +0000

    modified to work on prod

--- applications/nutritionist_app/nutritionist-app-backend/app/main.py
+++ applications/nutritionist_app/nutritionist-app-backend/app/main.py
@@ -5,9 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://ec2-54-152-228-191.compute-1.amazonaws.com:5173",
+    "http://54.152.228.191:5173"  # If your frontend can be served from this IP as well
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
